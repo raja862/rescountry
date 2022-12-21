780dbd99f57bbc91098689a04f940d8ca5780268
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2022-12-21 13:14:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let request=new XMLHttpRequest();
+
+request.open("GET","https://restcountries.com/v3.1/all")
+
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    for(var i=0;i<result.length;i++)
+    console.log(result[i].flags.png);
+    
+}
+
+
+let requests=new XMLHttpRequest();
+
+requests.open("GET","https://restcountries.com/v3.1/all")
+
+requests.send();
+requests.onload=function(){
+    var result=JSON.parse(requests.response);
+    for(var i=0;i<result.length;i++)
+    
+    console.log(result[i].name);
+}
